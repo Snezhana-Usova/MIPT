474332e95aa56ff9445b9dea23b2a4ca3681b138
Snezhana-Usova <60842269+Snezhana-Usova@users.noreply.github.com>
2021-05-10 23:11:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import jaydebeapi
 from py_scripts.my_module import declutter_stg, insert_stg, insert_scd2, merge_scd2, upd_meta
 
 # Установка соединения с сервером
-conn = jaydebeapi.connect('oracle.jdbc.driver.OracleDriver','jdbc:oracle:thin:demipt/gandalfthegrey@de-oracle.chronosavant.ru:1521/deoracle',['demipt','gandalfthegrey'],'/home/demipt/ojdbc8.jar')
+conn = jaydebeapi.connect('oracle.jdbc.driver.OracleDriver','jdbc:oracle:thin:demipt/*********@d*******.******.ru:1521/*****',['******','******'],'/home/demipt/ojdbc8.jar')
 conn.jconn.setAutoCommit(False)
 curs = conn.cursor()
 
